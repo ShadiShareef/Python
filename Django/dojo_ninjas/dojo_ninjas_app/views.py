cac0c2006290c78f983a2c3695cdde0189bf2958
Shadi Shareef <SHadi.elec1994@Gmail.com>
2022-04-18 02:59:06 +0300

    Dojo & Ninja assignment

--- Django/dojo_ninjas/dojo_ninjas_app/views.py
+++ Django/dojo_ninjas/dojo_ninjas_app/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from unicodedata import name
+from django.shortcuts import render, HttpResponse,redirect
+from .models import Dojo, Ninja
 
-# Create your views here.
+
+def index(request):
+    alldojo=Dojo.objects.all()
+    allninja=Ninja.objects.all()
+    context={
+        'alldojo':alldojo ,
+        'allninja': allninja
+    }
+    return render(request,"index.html",context)
+
+def addDojo(request):
+
+    dojoname=request.POST['name']
+    dojocity=request.POST['city']
+    dojostate=request.POST['state']
+    Dojo.objects.create(name=dojoname,city=dojocity , state = dojostate)
+    return redirect('/')
+
+def addNinja(request):
+
+    ninja_firstName=request.POST['firstName']
+    ninja_lastName=request.POST['lastName']
+    selectDojo=int(request.POST['dojo'])
+    ninja_dojo=Dojo.objects.get(id=selectDojo)
+    Ninja.objects.create(firstName=ninja_firstName,lastName=ninja_lastName ,dojo = ninja_dojo)
+
+    return redirect('/')
